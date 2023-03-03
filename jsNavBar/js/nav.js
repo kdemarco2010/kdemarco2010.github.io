456cc5c660a4b81802063f925713f91515e5ba42
kdemarco2010 <110508437+kdemarco2010@users.noreply.github.com>
2023-03-02 21:42:20 -0500

    update js nav

--- jsNavBar/js/nav.js
+++ jsNavBar/js/nav.js
@@ -17,3 +17,14 @@ page.addEventListener("click", function(e){
         toggleDropdown();
     }
 });
+
+// Mouse Follower Demo
+const follower = document.getElementById("follower");
+
+document.addEventListener("mousemove", function(e) {
+    let x = e.pageX -25;
+    let y = e.pageY -25;
+
+    follower.style.top = y + "px";
+    follower.style.left = x + "px";
+});
