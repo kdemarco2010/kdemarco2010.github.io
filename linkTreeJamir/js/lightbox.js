69b41aa51b58159c744b619369b0a588536a8be4
kdemarco2010 <110508437+kdemarco2010@users.noreply.github.com>
2023-06-05 11:07:55 -0400

    Jamir Linktree

---
+++ linkTreeJamir/js/lightbox.js
@@ -0,0 +1,73 @@
+
+let lightboxContainer = document.getElementById("lightboxContainer");
+let lightboxBackground = document.getElementById("lightboxBackground");
+let lightbox = document.getElementById("lightbox");
+let lightboxCloser = document.getElementById("lightboxCloser");
+
+function openLightbox() {
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let galleryImages = document.getElementsByClassName("galleryImage");
+let profilePopUp = document.getElementsByClassName("profileLightbox");
+
+for (let i = 0; i < galleryImages.length; i++) {
+    let galleryImage = galleryImages[i];
+    galleryImage.onclick = openLightbox;
+}
+
+for (let i = 0; i < profilePopUp.length; i++) {
+    let profileLightbox = profilePopUp[i];
+    profileLightbox.onclick = openLightbox;
+}
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+let closers = document.getElementsByClassName("closer");
+
+for (let i = 0; i < closers.length; i++) {
+    let closer = closers[i];
+    closer.onclick = closeLightbox;
+}
+
+// lightboxBackground.onclick = closeLightbox;
+// lightboxCloser.onclick = closeLightbox;
+
+// ------------ GALLERIES ----------------- //
+
+const client1Gallery = document.getElementById("client1Gallery");
+const client2Gallery = document.getElementById("client2Gallery");
+const client3Gallery = document.getElementById("client3Gallery");
+const client1 = document.getElementById("client1");
+const client2 = document.getElementById("client2");
+const client3 = document.getElementById("client3");
+
+function hideAll() {
+    client1Gallery.style.display = "none";
+    client2Gallery.style.display = "none";
+    client3Gallery.style.display = "none";
+}
+hideAll();
+
+function showClient1() {
+    hideAll();
+    client1Gallery.style.display = "";
+}
+client1.onclick = showClient1;
+
+
+function showClient2() {
+    hideAll();
+    client2Gallery.style.display = "";
+}
+client2.onclick = showClient2;
+
+function showClient3() {
+    hideAll();
+    client3Gallery.style.display = "";
+}
+client3.onclick = showClient3;
